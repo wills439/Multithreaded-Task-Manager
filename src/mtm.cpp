5b7b9ec62d01eae1506526fc46c008ff45083383
wills439 <willsullivan439@gmail.com>
2026-09-20 19:31:23 -0400

    Implemented basic threadpool class, task object container, and enum properties.

--- src/mtm.cpp
+++ src/mtm.cpp
@@ -1,6 +1,47 @@
 #include<iostream>
+#include<vector>
+#include<thread>
+#include<queue>
+
+enum class Status{Pending, Complete, Active};
+enum class Priority{Low, Medium, High};
+
+struct Task{
+    void(*task)();
+    Status taskStatus = Status::Pending;
+    Priority taskPriority;
+};
+
+class ThreadPool{
+    std::vector<std::thread> workers;
+    std::queue<Task> taskQeueu;
+    
+    void WorkerLoop(){
+        Task currentTask;
+        currentTask = taskQeueu.front();
+
+        currentTask.task();
+
+        taskQeueu.pop();
+    }
+
+
+    public:
+        ThreadPool(size_t threadNum){
+            workers.resize(threadNum);
+        }
+
+        void AddTaskToQueue(Task task){
+            taskQeueu.push(task);
+        }
+        
+        void SendTaskToWorker(){
+            workers.emplace_back(WorkerLoop);
+        }
+};
 
 int main(){
-    std::cout << "Hello World";
+    ThreadPool pool(8);
+    
     return 0;
 }
