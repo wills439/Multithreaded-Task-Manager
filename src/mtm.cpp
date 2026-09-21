207fd91bbc599923f20a047e1563ee6121e8586e
wills439 <willsullivan439@gmail.com>
2026-09-20 20:13:44 -0400

    Generic task function parameters in progress

--- src/mtm.cpp
+++ src/mtm.cpp
@@ -7,7 +7,8 @@ enum class Status{Pending, Complete, Active};
 enum class Priority{Low, Medium, High};
 
 struct Task{
-    void(*task)();
+    void(*task)(void*);
+    void* args;
     Status taskStatus = Status::Pending;
     Priority taskPriority;
 };
@@ -20,11 +21,10 @@ class ThreadPool{
         Task currentTask;
         currentTask = taskQeueu.front();
 
-        currentTask.task();
+        currentTask.task(nullptr);
 
         taskQeueu.pop();
     }
-
 
     public:
         ThreadPool(size_t threadNum){
@@ -36,9 +36,29 @@ class ThreadPool{
         }
         
         void SendTaskToWorker(){
-            workers.emplace_back(WorkerLoop);
+            workers.emplace_back(&ThreadPool::WorkerLoop, this);
         }
 };
+
+void ComputePrimes(void* args){
+    //make read args void ptr
+    
+    std::vector<int> primes;
+    for(size_t i = 2; i < args->end; i++){
+        int isPrime = 1;
+        for(size_t j = i; j > 0; j--){
+            if(i%j == 0 && i != j && j != 1){
+                isPrime = 0;
+            }
+        }
+
+        if(isPrime)
+            primes.push_back(i);
+    }
+
+    for(int i = 0; i < primes.size(); i++)
+        std::cout << primes[i] << "\n";
+}
 
 int main(){
     ThreadPool pool(8);
