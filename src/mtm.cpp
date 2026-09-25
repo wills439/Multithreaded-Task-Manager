d76c929041e380390c782a1ee94da9a63c095184
wills439 <willsullivan439@gmail.com>
2026-09-25 18:52:06 -0400

    Generic function pass implemented

--- src/mtm.cpp
+++ src/mtm.cpp
@@ -2,13 +2,13 @@
 #include<vector>
 #include<thread>
 #include<queue>
+#include<functional>
 
 enum class Status{Pending, Complete, Active};
 enum class Priority{Low, Medium, High};
 
 struct Task{
-    void(*task)(void*);
-    void* args;
+    std::function<void()> task;
     Status taskStatus = Status::Pending;
     Priority taskPriority;
 };
@@ -21,37 +21,29 @@ class ThreadPool{
         Task currentTask;
         currentTask = taskQeueu.front();
 
-        currentTask.task(nullptr);
+        currentTask.task();
 
         taskQeueu.pop();
     }
 
-    public:
-        ThreadPool(size_t threadNum){
-            workers.resize(threadNum);
-        }
-
-        void AddTaskToQueue(Task task){
-            taskQeueu.push(task);
-        }
-        
+    private:
         void SendTaskToWorker(){
             workers.emplace_back(&ThreadPool::WorkerLoop, this);
         }
+
+    public:
+        void AddTaskToQueue(Task task){
+            taskQeueu.push(task);
+        }
 };
 
-void ComputePrimes(void* args){
-    //make read args void ptr
-    
+void ComputePrimes(size_t end){
     std::vector<int> primes;
-    for(size_t i = 2; i < args->end; i++){
+    for(size_t i = 2; i < end; i++){
         int isPrime = 1;
-        for(size_t j = i; j > 0; j--){
-            if(i%j == 0 && i != j && j != 1){
+        for(size_t j = i; j > 0; j--)
+            if(i%j == 0 && i != j && j != 1)
                 isPrime = 0;
-            }
-        }
-
         if(isPrime)
             primes.push_back(i);
     }
@@ -61,7 +53,12 @@ void ComputePrimes(void* args){
 }
 
 int main(){
-    ThreadPool pool(8);
+    ThreadPool pool;
+
+    Task primes_one;
+    primes_one.task = [](){ ComputePrimes(1000);};
+
+    pool.AddTaskToQueue(primes_one);
     
     return 0;
 }
